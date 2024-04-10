69966fa08c89d2898131034091cd704227154c26
Dias Karshal <87409415+dkarshal@users.noreply.github.com>
2024-04-10 18:42:52 +0500

    updateee

--- data.sql
+++ data.sql
@@ -1,27 +1,17 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
+-- phpMyAdmin SQL
 -- Host: 127.0.0.1
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+05:00";
 
 -- Database: `data`
-
--- Table structure for table `db`
+-- Table `db`
 
 CREATE TABLE `tb_data` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL PRIMARY KEY,
   `name` varchar(50) NOT NULL,
   `comment` varchar(150) NOT NULL,
   `palette` varchar(150) NOT NULL,
   `date` varchar(50) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-ALTER TABLE `db`
-  ADD PRIMARY KEY (`id`);
-
