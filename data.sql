5cfb180d08faf0c0fddd792d3df509ee15dc1b6c
Dias Karshal <87409415+dkarshal@users.noreply.github.com>
2024-04-23 12:36:29 +0500

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,17 +1,61 @@
--- phpMyAdmin SQL
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
 -- Host: 127.0.0.1
+-- Generation Time: Apr 23, 2024 at 09:35 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+05:00";
+SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
 -- Database: `data`
--- Table `db`
+--
 
-CREATE TABLE `tb_data` (
-  `id` int(11) NOT NULL PRIMARY KEY,
-  `name` varchar(50) NOT NULL,
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `db`
+--
+
+CREATE TABLE `db` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL DEFAULT 'Guest',
   `comment` varchar(150) NOT NULL,
   `palette` varchar(150) NOT NULL,
-  `date` varchar(50) NOT NULL,
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `date` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `db`
+--
+ALTER TABLE `db`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `db`
+--
+ALTER TABLE `db`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=111;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
